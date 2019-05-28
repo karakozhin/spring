6c4867a59b3d61b2a08f928e17d097df89ea6408
zh.karakozhin <zh.karakozhin@statdata.kz>
2019-05-28 18:35:17 +0600

    added react js

---
+++ slideshow/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Соеденяем Java + ReactJS</p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
